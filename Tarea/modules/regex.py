d19efb48a81a90dd185ee9ef1d4a9d4e075c7c19
Matias del Barco <130382042+mdelBarco@users.noreply.github.com>
2024-06-23 20:00:09 -0400

    Actualización Regex

--- Tarea/modules/regex.py
+++ Tarea/modules/regex.py
@@ -1,13 +1,20 @@
+# Se importa la libreria re, para poder identificar expresiones regulares.
+from typing import NamedTuple
 import re
 
-
-
+class Token(NamedTuple):
+    type: str
+    value: str
+    line: int
+    column: int
+    
 class Regex:
     """
     Clase Regex de Regular Expresions que inicializa los lexemas a evaluar.
     """
-
+    
     def __init__(self):
+        # Se definen los lexemas a evaluar.
         self.compLexicos = [
             ("NEW_LINE", r'\n'),
             ("L_CORCHETE", r'\['),
@@ -21,16 +28,15 @@ class Regex:
             ("PR_TRUE", r'true|TRUE'),
             ("PR_FALSE", r'false|FALSE'),
             ("PR_NULL", r'null|NULL'),
-            ("SKIP", r'[ \t]+')
+            ("SKIP", r'[ \t]+'),
+            ("ESPACIO", r' ')
         ]
-
-
-
+    
     def getLexema (self, input):
         """
         Se recorre el array de tuplas en donde estan definidos
-        los lexemas, y por cada token recibido, verifica si
-        su expresion regular coincide con el código fuente.
+        los lexemas, y por cada token recibido, verifica si su
+        expresion regular coincide con el codigo fuente.
         """
         errores = []
         resultado = ""
@@ -38,12 +44,12 @@ class Regex:
         try:
             token_regex = '|'.join('(?P<%s>%s)' % par for par in self.compLexicos)
             numero_linea = 1 
-            inicio_linea = 0 
+            inicio_linea = 0
 
             for token in re.finditer(token_regex, input):
                 tipoLexema = token.lastgroup
                 valorLexema = token.group()
-
+    
                 if(tipoLexema == "NEW_LINE"):
                     numero_linea += 1
                     inicio_linea = token.end()
@@ -63,7 +69,7 @@ class Regex:
                     continue
                 elif (tipoLexema not in tipos):
                     errores.append('Error, ', valorLexema, ' no específicado entre los lexemas encontrado en la linea: ',numero_linea)
-
+            
             if(len(errores) > 0):
                 raise('Errores durante la ejecución: ' + '\n'.join(errores))
             else:
